1132e0ce5fd03a8fa754ef5c7ce395505a8d9349
K-Summer <91466399+K-Summer@users.noreply.github.com>
2025-08-01 20:12:50 +0800

    Create 0.py

---
+++ 0.py
@@ -0,0 +1,43 @@
+import requests
+
+
+def generate_completion(prompt, model="monika:latest"):
+    url = "http://localhost:11434/api/generate"
+    headers = {"Content-Type": "application/json"}
+    data = {"model": model, "prompt": prompt, "stream": False}
+
+    response = requests.post(url, headers=headers, json=data)
+    return response.json().get("response", "")
+
+
+text = input("输入文本：")
+completion = generate_completion(text)
+print("LocalTalk：", completion)
+
+# 文本转语音服务的URL
+tts_url = "http://localhost:9880"
+
+
+def test_tts_service(text):
+    tts_response = requests.get(
+        tts_url,
+        params={
+            "refer_wav_path": "参考音频",
+            "prompt_text": "参考文本",
+            "prompt_language": "zh",
+            "text": text,
+            "text_language": "zh",
+        },
+    )
+
+    if tts_response.status_code == 200:
+        with open("output_audio.wav", "wb") as f:
+            f.write(tts_response.content)
+        print("音频文件已成功保存为 output_audio.wav")
+    else:
+        print(f"Error in TTS service: {tts_response.text}")
+
+
+# 测试文本
+
+test_tts_service(completion)
